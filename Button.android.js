86bde58126113dc4e23cb1bd8e6f5256a836aa28
lichengzhi <lichengzhi@cnicg.cn>
2019-08-29 17:35:08 +0800

    change button type & add underlinewidth

--- Button.android.js
+++ Button.android.js
@@ -1,18 +1,17 @@
-const React = require('react');
-const ReactNative = require('react-native');
-const {
-  TouchableNativeFeedback,
-  View,
-} = ReactNative;
+const React = require("react");
+const ReactNative = require("react-native");
+const { TouchableNativeFeedback, TouchableWithoutFeedback, View } = ReactNative;
 
-const Button = (props) => {
-  return <TouchableNativeFeedback
-    delayPressIn={0}
-    background={TouchableNativeFeedback.SelectableBackground()} // eslint-disable-line new-cap
-    {...props}
-  >
-    {props.children}
-  </TouchableNativeFeedback>;
+const Button = props => {
+  return (
+    <TouchableWithoutFeedback
+      delayPressIn={0}
+      background={TouchableNativeFeedback.SelectableBackground()} // eslint-disable-line new-cap
+      {...props}
+    >
+      {props.children}
+    </TouchableWithoutFeedback>
+  );
 };
 
 module.exports = Button;
